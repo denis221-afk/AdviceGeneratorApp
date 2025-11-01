983efad3d58cc162da0cc101f76ea7927424764b
Svitlana Herc <den4ik.mazuryk@gmail.com>
2025-11-01 18:36:53 +0100

    last2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,45 @@
-import 'normalize.css';
-import './Styles/Main.css';
-import './Scripts/Count';
-import './Scripts/ApiGep'
+const isCaunt = document.querySelector('#isCount'), 
+      isBtn = document.querySelector('#btn');
+      isContent = document.querySelector(`#content`);
+
+function trigerCaunt() {
+    let caunt = 0;
+
+    return function changeCaunt () {
+        caunt++
+        return caunt
+    }
+    
+}
+
+const handleCaunt = trigerCaunt();
+
+
+isBtn.addEventListener('click', () => {
+    isCaunt.innerHTML = `Advice#${handleCaunt().toString()}`;
+})
+
+
+     
+
+
+
+function getRandomAdvice() {
+    fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(res => pushAdvice(res.slip.advice))
+    .catch(err => console.log(err))
+}
+
+
+
+ getRandomAdvice(); 
+
+ function pushAdvice (text) {
+    isContent.innerHTML = text; 
+}
+
+isBtn.addEventListener('click', getRandomAdvice)
+
 
 
